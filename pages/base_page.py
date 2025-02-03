81fca9febda9f9589dfe0fe6b8870dc4e235af6e
Georgiy Malinovskiy <11003995+GeorgiyMalinovskiy@users.noreply.github.com>
2025-02-03 10:14:53 +0300

    modified:   __init__.py deleted:    conftest.py new file:   pages/__init__.py modified:   pages/base_page.py new file:   tests/__init__.py new file:   tests/data.py modified:   tests/test_order.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
@@ -6,14 +7,18 @@ class BasePage:
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
 
+    @allure.step('Клик по элементу {locator}')
     def click(self, locator):
         self.wait.until(expected_conditions.element_to_be_clickable(locator)).click()
 
+    @allure.step('Поиск элемента {locator}')
     def find_element(self, locator):
         return self.wait.until(expected_conditions.presence_of_element_located(locator))
 
+    @allure.step('Проверка видимости элемента {locator}')
     def is_element_visible(self, locator):
         return self.wait.until(expected_conditions.visibility_of_element_located(locator))
 
+    @allure.step('Ввод текста в элемент {locator}')
     def send_keys(self, locator, text):
-        self.wait.until(expected_conditions.presence_of_element_located(locator)).send_keys(text) 
+        self.wait.until(expected_conditions.presence_of_element_located(locator)).send_keys(text)
--- tests/test_order.py
+++ tests/test_order.py
@@ -1,39 +1,14 @@
 import pytest
+import allure
+
 from pages.home_page import HomePage
 from pages.order_page import OrderPage
-import allure
-from datetime import datetime, timedelta
-
+from tests.data import test_data
 class TestScooterOrder:
     
     @allure.feature('Заказ самоката')
     @allure.story('Позитивный сценарий заказа')
-    @pytest.mark.parametrize('button_position, user_data, rent_data', [
-        ('top', {
-            'name': 'Иван',
-            'surname': 'Иванов',
-            'address': 'ул. Ленина, 1',
-            'metro': 'Сокольники',
-            'phone': '+79991234567'
-        }, {
-            'date': (datetime.now() + timedelta(days=1)).strftime('%d.%m.%Y'),
-            'rental_period': 'сутки',
-            'color': 'black',
-            'comment': 'Позвоните за час до доставки'
-        }),
-        ('bottom', {
-            'name': 'Петр',
-            'surname': 'Петров',
-            'address': 'ул. Пушкина, 10',
-            'metro': 'Лубянка',
-            'phone': '+79997654321'
-        }, {
-            'date': (datetime.now() + timedelta(days=2)).strftime('%d.%m.%Y'),
-            'rental_period': 'двое суток',
-            'color': 'grey',
-            'comment': 'Домофон не работает, звоните'
-        })
-    ])
+    @pytest.mark.parametrize('button_position, user_data, rent_data', test_data)
     def test_order_flow(self, driver, button_position, user_data, rent_data):
         home_page = HomePage(driver)
         order_page = OrderPage(driver)
